c901dd1b1dd1e0e598587fc197ed51aa59c21248
alysha.reinard@gmail.com <alysha.reinard@noaa.gov>
2020-01-22 12:16:26 +0100

    completed project

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,9 +9,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var wingame = Int.random(in: 0...1)
+    @State var objective = ["WIN", "LOSE"]
+    @State var compMove = Int.random(in: 0...2)
+    @State var possMoves = ["RPS_scissors", "RPS_rock", "RPS_paper"]
+    @State var scoreTitle = ""
+    @State var score = 0
+    
+    
     var body: some View {
-        Text("Hello, World!")
+
+        VStack{
+            Spacer()
+            Text("Roshambo brain bender")
+            .font(Font.custom("Arial Rounded MT Bold", size:24))
+
+            Spacer()
+            
+            Text("Computer's move:")
+            Image(self.possMoves[compMove])
+                // .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text("You want to : \(objective[wingame])")
+            
+            HStack{
+                ForEach(0 ..< 3){ move in
+                    Button(action: {
+                        self.moveTapped(move)
+                    }) {
+                        Image(self.possMoves[move])
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                Spacer()
+                
+                
+            }//Hstack
+            Text("\(scoreTitle)")
+            Text("Your score is: \(score)")
+            Spacer()
+            
+        }//Vstack
+
+        
+    }//body
+    func nextMove(){
+        wingame = Int.random(in: 0...1)
+        compMove = Int.random(in: 0...2)
     }
+    func correctMove()->Int{
+        if self.wingame == 0 {
+            return ((self.compMove + 1) % 3)
+        }else{
+            return((self.compMove - 1) % 3)
+            
+        }
+        
+    }//func correctMove
+    
+    func moveTapped(_ move:Int){
+        if move == correctMove(){
+            scoreTitle = "Correct!"
+            score+=1
+            
+        }else{
+            scoreTitle = "Wrong!"
+        }
+        nextMove()
+    }//func moveTapped
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
